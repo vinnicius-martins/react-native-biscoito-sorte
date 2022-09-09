69711c89efd7140683904770c9911fb51a291054
vinnicius <vinniciusmartins@dpc.com.br>
2022-09-09 15:25:20 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 
-export default function App() {
+const App = () => {
+
+  const [frase, setFrase] = useState('Clique no botão abaixo para abrir seu biscoito da sorte')
+  const [imgBiscoito, setImgBiscoito] = useState(require('./src/biscoito.png'))
+
+  const FRASES = ['A vida trará coisas boas se tiver paciência.',
+    'Demonstre amor e alegria em todas as oportunidades e verá que a paz nasce dentro de si.',
+    'Não compense na ira o que lhe falta na razão.',
+    'Defeitos e virtudes são apenas dois lados da mesma moeda.',
+    'A maior de todas as torres começa no solo.',
+    'Não há que ser forte. Há que ser flexível.',
+    'Todos os dias organiza os seus cabelos, por que não faz o mesmo com o coração.',
+    'Há três coisas que jamais voltam; a flecha lançada, a palavra dita e a oportunidade perdida.',
+    'A juventude não é uma época da vida, é um estado de espírito.',
+    'Podemos escolher o que semear, mas somos obrigados a colher o que plantamos.',
+    'Dê toda a atenção á formação dos seus filhos, sobretudo com bons exemplos da sua própria vida.',
+    'Siga os bons e aprenda com eles.',
+    'Não importa o tamanho da montanha, ela não pode tapar o sol.',]
+
+  const quebraBiscoito = () => {
+    const numeroAleatorio = Math.floor(Math.random() * FRASES.length)
+
+    setImgBiscoito(require('./src/biscoitoAberto.png'))
+    setFrase(`" ${FRASES[numeroAleatorio]} "`)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Image
+        source={imgBiscoito}
+        style={styles.img}
+      />
+
+      <Text style={styles.textoFrase}>{frase}</Text>
+
+      <TouchableOpacity style={styles.botao} onPress={quebraBiscoito} >
+        <View style={styles.btnArea}>
+          <Text style={styles.btnTexto}>Quebrar biscoito</Text>
+        </View>
+      </TouchableOpacity>
+
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  img: {
+    width: 250,
+    height: 250
+  },
+  textoFrase: {
+    fontSize: 20,
+    color: '#dd7b22',
+    margin: 30,
+    fontStyle: 'italic',
+    textAlign: 'center',
+  },
+  botao: {
+    width: 230,
+    height: 50,
+    borderWidth: 2,
+    borderColor: '#dd7b22',
+    borderRadius: 25
+  },
+  btnArea: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  btnTexto: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#dd7b22',
+  }
+})
+
+export default App
